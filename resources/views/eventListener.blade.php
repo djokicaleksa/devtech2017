a9534e28f23b521712634ff9a464c267fdf0b7a4
Aleksa <djokicaleksa@gmail.com>
2017-11-03 20:36:48 +0100

    init

---
+++ resources/views/eventListener.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<title></title>
+</head>
+<body>
+
+</body>
+<script type="text/javascript" src="js/app.js"></script>
+</html>
