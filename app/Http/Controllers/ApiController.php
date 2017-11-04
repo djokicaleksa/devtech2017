ab3971e15fbed319b0bc256ba2d56e580490222b
Aleksa <djokicaleksa@gmail.com>
2017-11-04 18:51:15 +0100

    asdf

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -45,6 +45,11 @@ class ApiController extends Controller
 
     public function recycle(Request $request)
     {
+        $input['user_id'] = $request->input('user_id');
+        $input['bin_id'] = $request->input('bin_id');
+        $input['token'] = $request->input('token');
+        $input['card_id'] = $request->input('card_id');
+
     	$user = User::findOrFail($input['user_id']);
 
         if($user == null){
