f8fe540b9443729b2a72e3dc23ce93d8a0977fda
Aleksa <djokicaleksa@gmail.com>
2017-11-04 01:38:17 +0100

    balance default value

--- database/migrations/2017_11_03_223731_add_balance_column_to_users.php
+++ database/migrations/2017_11_03_223731_add_balance_column_to_users.php
@@ -14,7 +14,7 @@ class AddBalanceColumnToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->double('balance');
+            $table->double('balance')->default(0);
         });
     }
 
