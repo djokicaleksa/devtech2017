fa62103020e816f78280b123d316736a026ae1ea
Aleksa <djokicaleksa@gmail.com>
2017-11-04 19:27:38 +0100

    fdgdf

--- routes/api.php
+++ routes/api.php
@@ -25,8 +25,7 @@ Route::middleware(['apiauth'])->group(function(){
 
 	Route::get('/barcode/{token}/{bin_id}/{barcode}', 'ApiController@getProduct');
 
-	// Route::post('recycle', 'ApiController@recycle');
-	Route::post('recycle/{token}{bin_id}/{user_id}{barcode_id}', 'ApiController@recycle');
+	Route::post('recycle', 'ApiController@recycle');
 });
 
 Route::get('card-block-stats', 'ApiController@cardBlockStats');
